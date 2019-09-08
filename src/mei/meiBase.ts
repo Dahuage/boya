260995a14cf378404a6150ba7cd02ae43b008104
刘华 <guzhaoer@gmail.com>
2019-09-08 22:14:09 +0800

    commit

---
+++ src/mei/meiBase.ts
@@ -0,0 +1,10 @@
+/**
+ * Base class of attribute and element.
+ * @author dahua<guzhaoer@gmail.com>
+ */
+
+export namespace meiBase {
+    export abstract class Attribute {}
+    export abstract class Object {}
+    export abstract class Element {}
+}
---
+++ src/mei/modules/shared.ts
@@ -0,0 +1,14 @@
+/**
+ *  MEI shared module implement.
+ */
+
+/// ../meiBase.ts
+
+export interface AccidInterface {};
+export class Accid extends Element implements AccidInterface {};
+
+export interface ActorInterface {};
+export class Actor extends Element implements AccidInterface {};
+
+export interface AddrLineInterface {};
+export class AddrLine extends Element implements AddrLineInterface{};
